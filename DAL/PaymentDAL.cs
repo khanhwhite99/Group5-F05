63ae43849ad7db2944ebcb726e4491015b7ca116
khanhwhite99 <khanhtn.nde17049@vtc.edu.vn>
2018-07-22 09:15:32 +0700

    [ADD] class palyment dal

---
+++ DAL/PaymentDAL.cs
@@ -0,0 +1,42 @@
+
+using System.Collections.Generic;
+using Persistence;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    class PaymentDAL
+    {
+       static public List<Payment> GetPaymentByUserId(int id)
+        {
+           List<Payment> result = new List<Payment>();
+            using(MySqlConnection connection = DbConfiguration.OpenConnection())
+            {
+                string query = $"select * from payment where user_id = {id};";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                using(MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    
+                    while(reader.Read())
+                    {
+                        Payment payment = new Payment()
+                        {
+                            ID = reader.GetInt32("payment_id"),
+                            Name = reader.GetString("name"),
+                            Money = reader.GetDouble("money")
+                        };
+                        result.Add(payment);
+                    }
+                }
+            }
+            return result;
+        }
+        static bool CheckPayment(Payment payment, double money)
+        {
+            if(payment.ID == 1)
+                return (payment.Money >= money);
+            else
+                return false;
+        } 
+    }
+}
